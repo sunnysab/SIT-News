02beed3d0cc65818a6bd56ac4a25ac415f299e69
sunnysab <sunnysab@yeah.net>
2021-01-18 10:34:09 +0800

    更新 Controller，支持跨域 *

--- src/main/java/Service/Controller/NewsSearchController.java
+++ src/main/java/Service/Controller/NewsSearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/news")
 public class NewsSearchController {
 
